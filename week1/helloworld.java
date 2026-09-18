98fcc35a58c20f7afd2f9b7ec9f1a9481053f4d7
Yussuf-Abdirizak <ayussuf1408@stu.riarauniversity.ac.ke>
2026-09-18 06:49:52 +0300

    initial commit: Hello World program

---
+++ week1/helloworld.java
@@ -0,0 +1,9 @@
+
+public class helloworld {
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+System.out.println("My name is: Yussuf Abdirizak");
+System.out.println("I am learning Java at Riara University");
+System.out.println("Today's date is: 18th September 2026");
+}
+}
